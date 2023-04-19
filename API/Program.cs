c2af0935dc60548e76ca371b3a9b0540cf97ff5b
mldamico <matias.lioneldamico@gmail.com>
2023-04-19 18:07:13 -0300

    Added bootstrap and font Awesome

--- API/Program.cs
+++ API/Program.cs
@@ -9,13 +9,17 @@ builder.Services.AddControllers();
 
 builder.Services.AddDbContext<DataContext>(opt =>
 {
-  opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+
+builder.Services.AddCors();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-
+app.UseCors(
+    builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200")
+);
 
 app.MapControllers();
 
