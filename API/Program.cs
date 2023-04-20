3e2720d196161b051cdac54362a14c3614c6a29f
mldamico <matias.lioneldamico@gmail.com>
2023-04-20 17:51:21 -0300

    Login, register Dto and controller and createToken function

--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,6 @@
 using API.data;
+using API.Interfaces;
+using API.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,6 +15,7 @@ builder.Services.AddDbContext<DataContext>(opt =>
 });
 
 builder.Services.AddCors();
+builder.Services.AddScoped<ITokenService, TokenService>();
 
 var app = builder.Build();
 
