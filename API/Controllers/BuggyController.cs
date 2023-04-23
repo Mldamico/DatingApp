f3e8b3e9715f66dfba66f4e95452832eba5abe39
mldamico <matias.lioneldamico@gmail.com>
2023-04-23 19:37:20 -0300

    Error Middleware

---
+++ API/Controllers/BuggyController.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.data;
+using API.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class BuggyController : BaseApiController
+    {
+        private readonly DataContext _context;
+
+        public BuggyController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [Authorize]
+        [HttpGet("auth")]
+        public ActionResult<string> GetSecret()
+        {
+            return "Secret text";
+        }
+
+        [Authorize]
+        [HttpGet("not-found")]
+        public ActionResult<AppUser> GetNotFound()
+        {
+            var thing = _context.Users.Find(-1);
+            if (thing == null)
+                return NotFound();
+
+            return thing;
+        }
+
+        [Authorize]
+        [HttpGet("server-error")]
+        public ActionResult<string> GetServerError()
+        {
+            try
+            {
+                var thing = _context.Users.Find(-1);
+                var thingToReturn = thing.ToString();
+
+                return thingToReturn;
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "something wrong happened");
+            }
+        }
+
+        [Authorize]
+        [HttpGet("bad-request")]
+        public ActionResult<string> GetBadRequest()
+        {
+            return BadRequest("This was not a good response");
+        }
+    }
+}
