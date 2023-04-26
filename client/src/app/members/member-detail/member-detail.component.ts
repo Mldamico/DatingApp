a7a40fbd45d35ca1479b2da85e5e6a23f8b293d2
mldamico <matias.lioneldamico@gmail.com>
2023-04-26 17:09:12 -0300

    card to member routing

--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Member } from 'src/app/_models/member';
+import { MembersService } from 'src/app/_services/members.service';
 
 @Component({
   selector: 'app-member-detail',
   templateUrl: './member-detail.component.html',
-  styleUrls: ['./member-detail.component.css']
+  styleUrls: ['./member-detail.component.css'],
 })
 export class MemberDetailComponent implements OnInit {
-
-  constructor() { }
+  member: Member | undefined;
+  constructor(
+    private membersService: MembersService,
+    private router: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    this.loadMember();
   }
 
+  loadMember() {
+    const username = this.router.snapshot.paramMap.get('username');
+    if (!username) return;
+    this.membersService.getMember(username).subscribe({
+      next: (member) => (this.member = member),
+    });
+  }
 }
