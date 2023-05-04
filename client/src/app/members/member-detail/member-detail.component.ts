6675932574bcc05be2c0a34ae51640cdc1451e1c
mldamico <matias.lioneldamico@gmail.com>
2023-05-04 19:19:40 -0300

    Messages in client and deleting messages

--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -1,12 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import {
   NgxGalleryAnimation,
   NgxGalleryImage,
   NgxGalleryOptions,
 } from '@kolkov/ngx-gallery';
+import { TabDirective, TabsetComponent } from 'ngx-bootstrap/tabs';
+import { Message } from 'src/app/_models/Message';
 import { Member } from 'src/app/_models/member';
 import { MembersService } from 'src/app/_services/members.service';
+import { MessageService } from 'src/app/_services/message.service';
 
 @Component({
   selector: 'app-member-detail',
@@ -14,17 +17,29 @@ import { MembersService } from 'src/app/_services/members.service';
   styleUrls: ['./member-detail.component.css'],
 })
 export class MemberDetailComponent implements OnInit {
-  member: Member | undefined;
+  @ViewChild('memberTabs', { static: true }) memberTabs?: TabsetComponent;
+  member: Member = {} as Member;
+  activeTab?: TabDirective;
   galleryOptions: NgxGalleryOptions[] = [];
   galleryImages: NgxGalleryImage[] = [];
+  messages: Message[] = [];
 
   constructor(
     private membersService: MembersService,
-    private router: ActivatedRoute
+    private router: ActivatedRoute,
+    private messageService: MessageService
   ) {}
 
   ngOnInit(): void {
-    this.loadMember();
+    // this.loadMember();
+    this.router.data.subscribe({
+      next: (data) => (this.member = data['member']),
+    });
+    this.router.queryParams.subscribe({
+      next: (params) => {
+        params['tab'] && this.selectTab(params['tab']);
+      },
+    });
     this.galleryOptions = [
       {
         width: '500px',
@@ -35,6 +50,7 @@ export class MemberDetailComponent implements OnInit {
         preview: false,
       },
     ];
+    this.galleryImages = this.getImages();
   }
 
   getImages() {
@@ -50,14 +66,38 @@ export class MemberDetailComponent implements OnInit {
     return imageUrls;
   }
 
-  loadMember() {
-    const username = this.router.snapshot.paramMap.get('username');
-    if (!username) return;
-    this.membersService.getMember(username).subscribe({
-      next: (member) => {
-        this.member = member;
-        this.galleryImages = this.getImages();
-      },
-    });
+  // loadMember() {
+  //   const username = this.router.snapshot.paramMap.get('username');
+  //   if (!username) return;
+  //   this.membersService.getMember(username).subscribe({
+  //     next: (member) => {
+  //       this.member = member;
+  //       this.galleryImages = this.getImages();
+  //     },
+  //   });
+  // }
+
+  loadMessages() {
+    if (this.member) {
+      this.messageService.getMessageThread(this.member.userName).subscribe({
+        next: (messages) => {
+          this.messages = messages;
+          console.log(messages);
+        },
+        error: (error) => console.log(error),
+      });
+    }
+  }
+
+  onTabActivated(data: TabDirective) {
+    this.activeTab = data;
+    if (this.activeTab.heading === 'Messages') {
+      this.loadMessages();
+    }
+  }
+  selectTab(heading: string) {
+    if (this.memberTabs) {
+      this.memberTabs.tabs.find((x) => x.heading === heading)!.active = true;
+    }
   }
 }
